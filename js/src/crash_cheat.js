afe269b78e87736b0db5f9ae556dbfaf196e31b8
cesarexyh <yanghangxu@gmail.com>
2019-01-05 16:15:12 +0800

    Site updated: 2019-01-05 16:15:06

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '找不到页面了';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "../../images/favicon-16x16-next.png");
+         document.title = '哈哈又被你找到了' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
